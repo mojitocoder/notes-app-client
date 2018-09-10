4136ec4be69b940c8efc10c44440ff5d69143062
Quynh Nguyen <quynh.nguyen@simplybusiness.co.uk>
2018-09-10 16:25:56 +0100

    Add login page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,24 @@ import "./App.css";
 import Routes from "./Routes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
     return (
       <div className="App container">
         <Navbar fluid collapseOnSelect>
@@ -27,7 +44,7 @@ class App extends Component {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <Routes />
+        <Routes childProps={childProps} />
       </div>
     );
   }
