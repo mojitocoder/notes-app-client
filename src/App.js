3f60de6854648d3c5ea3ac09516ab0d501d38d93
Quynh Nguyen <quynh.nguyen@simplybusiness.co.uk>
2018-09-10 13:31:00 +0100

    Add Login/SignUp links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import { LinkContainer } from "react-router-bootstrap";
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 
@@ -11,10 +12,20 @@ class App extends Component {
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <Link to="/">Sratch</Link>
+              <Link to="/">Scratch</Link>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <LinkContainer to="/signup">
+                <NavItem>Signup</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <NavItem>Login</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Routes />
       </div>
